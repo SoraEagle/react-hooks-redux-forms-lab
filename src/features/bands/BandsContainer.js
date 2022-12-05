d57bc23a2bbfad5f5452fa46033d14703a276179
Matthew Phillips <phillips.matthew829@gmail.com>
2022-12-05 13:36:34 -0600

    commit tree 69dcb9acde7c865ba7c29b16df8cf1cde063bdae

--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,26 @@
 import React from "react";
+import {useSelector, useDispatch} from "react-redux";
+import BandInput from "./BandInput";
+import {bandAdded} from "./bandsSlice";
 
-function BandsContainer() {
-  return <div>BandsContainer</div>;
+function BandsContainer(){
+  const dispatch = useDispatch();
+  const bands = useSelector((state) => state.bands.entities);
+
+  function handleBandSubmit(name){
+    dispatch(bandAdded(name));
+  }
+  
+  return(
+    <div>
+      <BandInput onBandSubmit = {handleBandSubmit} />
+      <ul>
+        {bands.map((band) => (
+          <li key={band}>{band}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default BandsContainer;
